7f6f9f2d7855789c5215752960919bb2fa06b262
Marc Schwartz <marcschwartz1990@gmail.com>
2022-11-11 21:49:39 -0500

    Initial Commit

---
+++ MovieBrowserV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieBrowserV2
+//
+//  Created by Marc-Developer on 11/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
